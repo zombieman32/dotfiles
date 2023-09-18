73f68c013fbc5d6b3655dc3d20e899b2f7932991
zombieman32 <rafaelverde10@gmail.com>
2023-09-18 02:28:50 -0300

    Specified the onedark_vivid theme

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -77,7 +77,7 @@ end
 lualine.setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
+		theme = "onedark_vivid",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
