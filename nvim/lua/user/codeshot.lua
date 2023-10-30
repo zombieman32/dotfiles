74eaf39fd0b2c2efe4c01f1804c406490067871b
zombieman32 <rafaelverde10@gmail.com>
2023-10-30 18:39:19 -0300

    Added config for codeshot

--- nvim/lua/user/codeshot.lua
+++ nvim/lua/user/codeshot.lua
@@ -7,15 +7,38 @@ codeshot.setup({
 	output = function()
 		local core = require("core")
 		local buf_name = vim.api.nvim_buf_get_name(0)
-		return core.file.name(buf_name) .. ".png"
+		return "/hdd/Imágenes/Codeshot/" .. core.file.name(buf_name) .. ".png"
 	end,
 	options = function(select_area)
 		if not select_area then
-			return {}
-		end
+            return {
+				"--shadow-blur-radius",
+				"5",
+				"--no-line-number",
+				"--theme",
+				"OneHalfDark",
+				"--font",
+				"Fira Code",
+				"--background-image",
+				"/hdd/Imágenes/wp7074765.jpg",
+				"--line-pad",
+				"4"
+			}
+        end
 		return {
+			"--shadow-blur-radius",
+			"5",
 			"--line-offset",
 			select_area.s_start.row,
+			"--no-line-number",
+			"--theme",
+			"OneHalfDark",
+			"--font",
+			"Fira Code",
+			"--background-image",
+			"/hdd/Imágenes/wp7074765.jpg",
+			"--line-pad",
+			"4"
 		}
 	end,
 })
