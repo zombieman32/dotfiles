37443cae0fdd168763e526fe4874ad90bedad5dc
zombieman32 <rafaelverde10@gmail.com>
2023-11-28 01:41:01 -0300

    Turned on spell checking

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -11,6 +11,7 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
+		null_ls.builtins.completion.spell,
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
@@ -25,6 +26,8 @@ null_ls.setup({
 		formatting.fourmolu,
 		formatting.shfmt,
 		formatting.stylua,
+		formatting.astgrep,
+		formatting.gdformat,
 		diagnostics.flake8,
 		diagnostics.checkstyle,
 		diagnostics.cpplint,
@@ -41,5 +44,7 @@ null_ls.setup({
 		diagnostics.curlylint,
 		diagnostics.vint,
 		diagnostics.yamllint,
+		diagnostics.astgrep,
+		diagnostics.gdlint
 	},
 })
