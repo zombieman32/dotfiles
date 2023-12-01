93f1664758c3ff9bce3133266aad0076ccb61223
zombieman32 <rafaelverde10@gmail.com>
2023-12-01 11:40:51 -0300

    Cleaned code and changed folding method

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,53 +1,56 @@
-vim.opt.compatible = false						-- no vi compatibility
-vim.opt.backup = false                          -- creates a backup file
-vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
-vim.opt.completeopt = {"menu", "menuone", "noselect"  } -- mostly just for cmp
-vim.opt.conceallevel = 2                        -- so that `` is visible in markdown files
-vim.opt.concealcursor = "nc"
-vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
-vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
-vim.opt.ignorecase = true                       -- ignore case in search patterns
-vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
-vim.opt.pumheight = 10                          -- pop up menu height
-vim.opt.showtabline = 2                         -- always show tabs
-vim.opt.smartcase = true                        -- smart case
-vim.opt.smartindent = true                      -- make indenting smarter again
-vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
-vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
-vim.opt.swapfile = false                        -- creates a swapfile
-vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
-vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
-vim.opt.undofile = true                         -- enable persistent undo
-vim.opt.updatetime = 300                        -- faster completion (4000ms default)
-vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-vim.opt.expandtab = true                        -- convert tabs to spaces
-vim.opt.shiftwidth = 4                          -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4                             -- insert 4 spaces for a tab
-vim.o.cursorline = true                       -- highlight the current line
-vim.opt.cursorcolumn = false						-- highlight the current column
-vim.opt.relativenumber = true                  -- set relative numbered lines
-vim.opt.numberwidth = 2                         -- set number column width to 2 {default 4}
-vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.scrolloff = 10                           -- is one of my fav
-vim.opt.sidescrolloff = 8
-vim.opt.guifont = "FiraCode-Regular"                    -- the font used in graphical neovim applications
-vim.opt.showmode = true
-vim.opt.incsearch = true
-vim.opt.showcmd = true
-vim.opt.showmatch = true
-vim.opt.history = 1000
-vim.opt.wildmenu = true
-vim.opt.laststatus=2
+local vim = vim
+local o = vim.opt
+
+o.compatible = false						-- no vi compatibility
+o.backup = false                          -- creates a backup file
+o.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
+o.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+o.completeopt = {"menu", "menuone", "noselect"  } -- mostly just for cmp
+o.conceallevel = 2                        -- so that `` is visible in markdown files
+o.concealcursor = "nc"
+o.fileencoding = "utf-8"                  -- the encoding written to a file
+o.hlsearch = true                         -- highlight all matches on previous search pattern
+o.ignorecase = true                       -- ignore case in search patterns
+o.mouse = "a"                             -- allow the mouse to be used in neovim
+o.pumheight = 10                          -- pop up menu height
+o.showtabline = 2                         -- always show tabs
+o.smartcase = true                        -- smart case
+o.smartindent = true                      -- make indenting smarter again
+o.splitbelow = true                       -- force all horizontal splits to go below current window
+o.splitright = true                       -- force all vertical splits to go to the right of current window
+o.swapfile = false                        -- creates a swapfile
+o.termguicolors = true                    -- set term gui colors (most terminals support this)
+o.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
+o.undofile = true                         -- enable persistent undo
+o.updatetime = 300                        -- faster completion (4000ms default)
+o.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+o.expandtab = true                        -- convert tabs to spaces
+o.shiftwidth = 4                          -- the number of spaces inserted for each indentation
+o.tabstop = 4                             -- insert 4 spaces for a tab
+o.cursorline = true                       -- highlight the current line
+o.cursorcolumn = false						-- highlight the current column
+o.relativenumber = true                  -- set relative numbered lines
+o.numberwidth = 2                         -- set number column width to 2 {default 4}
+o.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
+o.wrap = false                            -- display lines as one long line
+o.scrolloff = 10                           -- is one of my fav
+o.sidescrolloff = 8
+o.guifont = "FiraCode-Regular"                    -- the font used in graphical neovim applications
+o.showmode = true
+o.incsearch = true
+o.showcmd = true
+o.showmatch = true
+o.history = 1000
+o.wildmenu = true
+o.laststatus=2
 vim.g.markdown_composer_autostart = 0
 vim.g.markdown_composer_browser = "epiphany"
 vim.loader.enable()
-vim.o.foldmetod = "syntax"
-vim.o.spelllang = "es_mx"
+o.foldmethod = "marker"
+o.spelllang = "es_mx"
 vim.o.bqn_prefix_key = "."
 
-vim.opt.shortmess:append "c"
+o.shortmess:append "c"
 
 vim.cmd "filetype on"
 vim.cmd "filetype plugin on"
@@ -55,4 +58,3 @@ vim.cmd "filetype indent on"
 vim.cmd "hi CursorLine cterm=NONE ctermbg=242"
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
