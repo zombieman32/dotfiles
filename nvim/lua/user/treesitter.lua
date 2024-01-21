e572b27c17d47d3b6897857518d3bcbb1a06f020
zombieman32 <rafaelverde10@gmail.com>
2024-01-21 01:00:20 -0300

    Removed rainbow-treesiter

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -16,15 +16,6 @@ configs.setup({
 		additional_vim_regex_highlighting = true,
 	},
 	indent = { enable = true, disable = { "yaml", "gdscript" } },
-	rainbow = {
-		enable = true,
-		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-		extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-		max_file_lines = nil, -- Do not enable for files with more than n lines, int
-		-- colors = {}, -- table of hex strings
-		-- termcolors = {} -- table of colour name strings
-		ensure_installed = { "org" }, -- Or run :TSUpdate org
-	},
 	textobjects = {
 		select = {
 			enable = true,
