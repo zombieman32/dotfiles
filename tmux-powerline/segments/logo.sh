51dd67b4334b8becb4a293c24e1c6bb43addfff5
zombieman32 <rafaelverde10@gmail.com>
2024-01-30 04:59:59 -0300

    Removed tmux-powerline

--- tmux-powerline/segments/logo.sh
+++
@@ -1,8 +0,0 @@
-run_segment() {
-    prefix_indicator="#[fg=#282c34]#{?client_prefix,#[bg=#e5c07b],#[bg=#89ca78]}"
-    prefix_indicator_right="#[bg=#ef596f]#{?client_prefix,#[fg=#e5c07b],#[fg=#89ca78]}"
-    prefix_indicator_left="#[fg=#282c34]#{?client_prefix,#[bg=#e5c07b],#[bg=#89ca78]}"
-
-    echo "${prefix_indicator}  ${prefix_indicator_right}"
-	return 0
-}
