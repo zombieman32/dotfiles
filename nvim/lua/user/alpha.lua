090a3018fa7dbef6ead19c5d7d7bb7bc3a0d4fab
zombieman32 <rafaelverde10@gmail.com>
2024-04-12 23:35:05 -0300

    Changed options to update pwd

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -219,8 +219,8 @@ dashboard.section.buttons.val = {
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "󰗧  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("l", "󰁯  Restore last session", ":lua require(\"persistence\").load({ last = true })<CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+	dashboard.button("l", "󰁯  Restore last session", ":lua require(\"persistence\").load({ last = true })<BAR> cd %:p:h <CR>"),
+	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <BAR> cd %:p:h <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
