8e6cf0d8b6ad3e620c1b218d857dc176fcbaf40b
zombieman32 <rafaelverde10@gmail.com>
2024-05-23 06:05:11 -0300

    nvim: Migrated from packer to lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,48 +1,70 @@
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.netrw_use_noswf= 0
+vim.g.netrw_use_noswf = 0
 vim.g.number = 1
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
-require("user.lazy")
-require("user.options")
-require("user.keymaps")
-require("user.plugins")
-require("user.colorscheme")
-require("user.nvimtree")
-require("user.cmp")
-require("user.lsp")
-require("user.cokeline")
-require("user.lualine")
-require("user.telescope")
-require("user.image")
-require("user.treesitter")
-require("user.autopairs")
-require("user.gitsigns")
-require("user.alpha")
-require("user.biscuits")
-require("user.toggleterm")
-require("user.masonti")
-require("user.multicursor")
-require("user.codeshot")
-require("user.regs")
-require("user.whichkey")
-require("user.autocmd")
-require("user.bqnlsp")
-require("user.bqnparser")
-require'lspconfig'.gdscript.setup{}
-require("user.linkvisitor")
-require("user.mini")
-require("user.null")
-require("user.nvim-ts-autotag")
-require("user.indent")
-require("user.catppuccin")
-require("user.octo")
-require("user.pretty-fold")
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    })
+end
 
-local mocha = require("catppuccin.palettes").get_palette "mocha"
-vim.api.nvim_set_hl(0, 'NormalMode', {fg = mocha.blue, bold = true})
-vim.api.nvim_set_hl(0, 'InsertMode', {fg = mocha.green, bold = true})
-vim.api.nvim_set_hl(0, 'ReplaceMode', {fg = mocha.red, bold = true})
-vim.api.nvim_set_hl(0, 'CommandMode', {fg = mocha.peach, bold = true})
-vim.api.nvim_set_hl(0, 'VisualMode', {fg = mocha.mauve, bold = true})
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('user.plugins', {
+    defaults = {
+        lazy = true,
+    },
+    ui = {
+        border = 'rounded',
+    },
+})
+require('user.options')
+require('user.keymaps')
+require('user.colorscheme')
+require('user.nvimtree')
+require('user.cmp')
+require('user.lsp')
+require('user.cokeline')
+require('user.lualine')
+require('user.telescope')
+require('user.image')
+require('user.treesitter')
+require('user.autopairs')
+require('user.gitsigns')
+require('user.alpha')
+require('user.biscuits')
+require('user.toggleterm')
+require('user.masonti')
+require('user.multicursor')
+require('user.codeshot')
+require('user.regs')
+require('user.whichkey')
+require('user.autocmd')
+require('user.bqnlsp')
+require('user.bqnparser')
+require('lspconfig').gdscript.setup({})
+require('user.linkvisitor')
+require('user.mini')
+require('user.null')
+require('user.nvim-ts-autotag')
+require('user.indent')
+require('user.catppuccin')
+require('user.octo')
+require('user.pretty-fold')
+
+local mocha = require('catppuccin.palettes').get_palette('mocha')
+vim.api.nvim_set_hl(0, 'NormalMode', { fg = mocha.blue, bold = true })
+vim.api.nvim_set_hl(0, 'InsertMode', { fg = mocha.green, bold = true })
+vim.api.nvim_set_hl(0, 'ReplaceMode', { fg = mocha.red, bold = true })
+vim.api.nvim_set_hl(0, 'CommandMode', { fg = mocha.peach, bold = true })
+vim.api.nvim_set_hl(0, 'VisualMode', { fg = mocha.mauve, bold = true })
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,150 +1,106 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system({
-        'git',
-        'clone',
-        '--depth',
-        '1',
-        'https://github.com/wbthomason/packer.nvim',
-        install_path,
-    })
-    print('Installing packer close and reopen Neovim...')
-    vim.cmd([[packadd packer.nvim]])
-end
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, 'packer')
-if not status_ok then
-    return
-end
-
--- Have packer use a popup window
-packer.init({
-    display = {
-        open_fn = function()
-            return require('packer.util').float({ border = 'rounded' })
-        end,
-    },
-})
-
--- Install your plugins here
-return packer.startup(function(use)
+return {
     -- My plugins here
-    use('wbthomason/packer.nvim') -- Have packer manage itself
-    use('nvim-lua/popup.nvim')    -- An implementation of the Popup API from vim in Neovim
-    use('nvim-lua/plenary.nvim')  -- Useful lua functions used ny lots of plugins
-    use('alvan/vim-closetag')     -- Autocloses tags
-    use('sheerun/vim-polyglot')
-    use('windwp/nvim-autopairs')
-    use({
+    {
+        '2kabhishek/nerdy.nvim',
+        dependencies = {
+            'stevearc/dressing.nvim',
+            'nvim-telescope/telescope.nvim',
+        },
+        cmd = 'Nerdy',
+    },
+    'nvim-lua/popup.nvim', -- An implementation of the Popup API from vim in Neovim
+    'nvim-lua/plenary.nvim', -- Useful lua functions used ny lots of plugins
+    'alvan/vim-closetag', -- Autocloses tags
+    'windwp/nvim-autopairs',
+    {
         'goolord/alpha-nvim',
         config = function()
             require('alpha').setup(require('alpha.themes.dashboard').config)
         end,
-    })                             -- Start screen
-    use('lewis6991/gitsigns.nvim') -- Git symbols
-    use({
+    }, -- Start screen
+    'lewis6991/gitsigns.nvim', -- Git symbols
+    {
         'willothy/nvim-cokeline',
-        requires = {
-            'nvim-lua/plenary.nvim',        -- Required for v0.4.0+
+        dependencies = {
+            'nvim-lua/plenary.nvim', -- Required for v0.4.0+
             'kyazdani42/nvim-web-devicons', -- If you want devicons
         },
-    })
-    use({
+    },
+    {
         'folke/persistence.nvim',
         event = 'BufReadPre', -- this will only start session saving when an actual file was opened
         module = 'persistence',
         config = function()
             require('persistence').setup()
         end,
-    })                                  -- Session manager
-    use('kyazdani42/nvim-web-devicons') -- Icons
-    use('moll/vim-bbye')
-    use({ 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } })
-    use('tamton-aquib/staline.nvim')   -- Status line and buffer line
-    use('norcalli/nvim-colorizer.lua') -- Color codes preview
-    use('lukas-reineke/indent-blankline.nvim')
-    use('HiPhish/rainbow-delimiters.nvim')
-    use({ 'nvim-tree/nvim-tree.lua', requires = 'nvim-tree/nvim-web-devicons' }) -- File Tree
-    use('euclio/vim-markdown-composer')
+    }, -- Session manager
+    'kyazdani42/nvim-web-devicons', -- Icons
+    'moll/vim-bbye',
+    { 'nvim-lualine/lualine.nvim', dependencies = { 'kyazdani42/nvim-web-devicons', opt = true } },
+    'tamton-aquib/staline.nvim', -- Status line and buffer line
+    'norcalli/nvim-colorizer.lua', -- Color codes preview
+    'lukas-reineke/indent-blankline.nvim',
+    'HiPhish/rainbow-delimiters.nvim',
+    { 'nvim-tree/nvim-tree.lua', dependencies = 'nvim-tree/nvim-web-devicons' }, -- File Tree
+    'euclio/vim-markdown-composer',
 
     -- Color themes
-    use('rebelot/kanagawa.nvim')
-    use('olimorris/onedarkpro.nvim')
-    use('folke/tokyonight.nvim')
-    use('catppuccin/nvim')
-    use('EdenEast/nightfox.nvim')
-    use('bluz71/vim-moonfly-colors')
-    use('savq/melange-nvim')
-    use('Mofiqul/dracula.nvim')
-    use('tiagovla/tokyodark.nvim')
+    { 'rebelot/kanagawa.nvim', lazy = true },
+    { 'olimorris/onedarkpro.nvim', lazy = true },
+    { 'folke/tokyonight.nvim', lazy = true },
+    {
+        'catppuccin/nvim',
+        dependencies = {
+            {
+                'mawkler/modicator.nvim',
+                config = function()
+                    require('modicator').setup({
+                        show_warnings = false,
+                        highlights = {
+                            italic = true,
+                        },
+                    })
+                end,
+            },
+        },
+        lazy = true,
+    },
+    { 'EdenEast/nightfox.nvim', lazy = true },
+    { 'bluz71/vim-moonfly-colors', lazy = true },
+    { 'savq/melange-nvim', lazy = true },
+    { 'Mofiqul/dracula.nvim', lazy = true },
+    { 'tiagovla/tokyodark.nvim', lazy = true },
 
     -- CMP
-    use('hrsh7th/cmp-buffer')       -- CMP buffer
-    use('hrsh7th/cmp-path')         -- CMP paths
-    use('hrsh7th/cmp-cmdline')      -- CMP completions
-    use('hrsh7th/nvim-cmp')         -- CMP main plugin
-    use('saadparwaiz1/cmp_luasnip') -- snippet completions
-    use('hrsh7th/cmp-nvim-lsp')
-    use('hrsh7th/cmp-nvim-lua')
+    'hrsh7th/cmp-buffer', -- CMP buffer
+    'hrsh7th/cmp-path', -- CMP paths
+    'hrsh7th/cmp-cmdline', -- CMP completions
+    'hrsh7th/nvim-cmp', -- CMP main plugin
+    'saadparwaiz1/cmp_luasnip', -- snippet completions
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lua',
 
     -- Snippets
-    use('L3MON4D3/LuaSnip')             --snippet engine
-    use('rafamadriz/friendly-snippets') -- a bunch of snippets to use
+    'L3MON4D3/LuaSnip', --snippet engine
+    'rafamadriz/friendly-snippets', -- a bunch of snippets to use
 
     -- LSP
-    use('neovim/nvim-lspconfig')             -- enable LSP
-    use('williamboman/mason.nvim')           -- simple to use language server installer
-    use('williamboman/mason-lspconfig.nvim') -- simple to use language server installer
-    use('nvimtools/none-ls.nvim')            -- LSP diagnostics and code actions
+    'neovim/nvim-lspconfig', -- enable LSP
+    'williamboman/mason.nvim', -- simple to use language server installer
+    'williamboman/mason-lspconfig.nvim', -- simple to use language server installer
+    'nvimtools/none-ls.nvim', -- LSP diagnostics and code actions
 
     -- Misc
-    use('andweeb/presence.nvim')  -- Discord integration
-    use('stevearc/dressing.nvim') -- Better UI
-    use({
+    'andweeb/presence.nvim', -- Discord integration
+    'stevearc/dressing.nvim', -- Better UI
+    {
         'smoka7/multicursors.nvim',
-        requires = {
+        dependencies = {
             'smoka7/hydra.nvim',
         },
-    })
-    use({
-        'samodostal/image.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim',
-        },
-    })
-    use('niuiic/core.nvim')
-    use({
-        'akinsho/toggleterm.nvim',
-        tag = '*',
-        config = function()
-            require('toggleterm').setup()
-        end,
-    })
-    use({
-        'mawkler/modicator.nvim',
-        after = 'onedarkpro.nvim', -- Add your colorscheme plugin here
-        config = function()
-            require('modicator').setup({
-                show_warnings = false,
-                highlights = {
-                    italic = true,
-                },
-            })
-        end,
-    })
-    use({
+    },
+    'niuiic/core.nvim',
+    {
         'ziontee113/icon-picker.nvim',
         config = function()
             require('icon-picker').setup({
@@ -152,10 +108,10 @@ return packer.startup(function(use)
             })
         end,
         opt = 'true',
-    })
-    use({
+    },
+    {
         'epwalsh/obsidian.nvim',
-        requires = {
+        dependencies = {
             -- Required.
             'nvim-lua/plenary.nvim',
             'hrsh7th/nvim-cmp',
@@ -166,56 +122,60 @@ return packer.startup(function(use)
                 dir = '~/my-vault',
             })
         end,
-    })
-    use({
+    },
+    {
         'code-biscuits/nvim-biscuits',
-        requires = {
+        dependencies = {
             'nvim-treesitter/nvim-treesitter',
-            run = ':TSUpdate',
+            build = ':TSUpdate',
         },
-    })
-    use('echasnovski/mini.nvim')
+    },
+    'echasnovski/mini.nvim',
     --Clojure
-    use('clojure-vim/acid.nvim')
-    use('Vigemus/impromptu.nvim')
-    use('clojure-vim/jazz.nvim')
+    'clojure-vim/acid.nvim',
+    'Vigemus/impromptu.nvim',
+    'clojure-vim/jazz.nvim',
 
     -- Telescope
-    use('nvim-telescope/telescope.nvim')
-    use('nvim-telescope/telescope-media-files.nvim')
+    'nvim-telescope/telescope.nvim',
+    'nvim-telescope/telescope-media-files.nvim',
 
     -- Treesitter
-    use({
+    {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-    })
-    use({
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        after = 'nvim-treesitter',
-        requires = 'nvim-treesitter/nvim-treesitter',
-    })
+        build = ':TSUpdate',
+        dependencies = {
+            {
+                'lukas-reineke/headlines.nvim',
+                config = function()
+                    require('headlines').setup()
+                end,
+            },
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+    },
 
     -- Tmux integration
-    use('christoomey/vim-tmux-navigator')
+    'christoomey/vim-tmux-navigator',
 
     -- Mason installer utility to ensure installed LSP's
-    use('WhoIsSethDaniel/mason-tool-installer.nvim')
-    use({ 'rcarriga/nvim-dap-ui', requires = { 'mfussenegger/nvim-dap' } }) -- Configs for DAP
-    use('jay-babu/mason-nvim-dap.nvim')                                     -- Better integration for Mason and nvim-dap
-    use('jay-babu/mason-null-ls.nvim')
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap' } }, -- Configs for DAP
+    'jay-babu/mason-nvim-dap.nvim', -- Better integration for Mason and nvim-dap
+    'jay-babu/mason-null-ls.nvim',
 
-    use('mbbill/undotree')
-    use('tidalcycles/vim-tidal')
+    'mbbill/undotree',
+    'tidalcycles/vim-tidal',
 
-    use('neomake/neomake')
+    'neomake/neomake',
 
-    use('tpope/vim-fugitive') -- Git commands
-    use('tpope/vim-sleuth')   -- Adjust shiftwidth and expantab based on current file
+    'tpope/vim-fugitive', -- Git commands
+    'tpope/vim-sleuth', -- Adjust shiftwidth and expantab based on current file
 
     -- Edit and review GitHub issues and pr's
-    use({
+    {
         'pwntester/octo.nvim',
-        requires = {
+        dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-telescope/telescope.nvim',
             'nvim-tree/nvim-web-devicons',
@@ -227,55 +187,48 @@ return packer.startup(function(use)
                 },
             })
         end,
-    })
+    },
 
     -- Show code context for large functions
-    use('nvim-treesitter/nvim-treesitter-context')
+    'nvim-treesitter/nvim-treesitter-context',
 
     -- Take shots of code
-    use({
+    {
         'niuiic/code-shot.nvim',
         require = {
             'niuiic/core.nvim',
         },
-    })
+    },
 
     -- Run blocks of code
-    use({ 'michaelb/sniprun', run = 'sudo sh ./install.sh' })
+    { 'michaelb/sniprun', build = 'sh ./install.sh' },
 
     -- Smart commenting
-    use({
+    {
         'numToStr/Comment.nvim',
         config = function()
             require('Comment').setup()
         end,
-    })
+    },
 
     -- OrgMode
-    use({
+    {
         'nvim-orgmode/orgmode',
         config = function()
             require('orgmode').setup({})
         end,
-    })
+    },
     -- Add utf-8 bullet points
-    use({
+    {
         'akinsho/org-bullets.nvim',
         config = function()
             require('org-bullets').setup()
         end,
-    })
+    },
     -- Highlights for markdown orgmode and neorg
-    use({
-        'lukas-reineke/headlines.nvim',
-        after = 'nvim-treesitter',
-        config = function()
-            require('headlines').setup()
-        end,
-    })
     -- Create tables
-    use('dhruvasagar/vim-table-mode')
-    use({
+    'dhruvasagar/vim-table-mode',
+    {
         'folke/which-key.nvim',
         config = function()
             vim.o.timeout = true
@@ -286,78 +239,65 @@ return packer.startup(function(use)
                 -- refer to the configuration section below
             })
         end,
-    })
-    use('habamax/vim-godot')                  -- Godot integration
-    use('uga-rosa/ccc.nvim')                  -- Color picker
-    use('https://git.sr.ht/~detegr/nvim-bqn') -- BQN utilities
-    use('xiyaowong/link-visitor.nvim')        -- Visit links in a file
-    use({
+    },
+    'habamax/vim-godot', -- Godot integration
+    'uga-rosa/ccc.nvim', -- Color picker
+    'https://git.sr.ht/~detegr/nvim-bqn', -- BQN utilities
+    'xiyaowong/link-visitor.nvim', -- Visit links in a file
+    {
         'stevearc/oil.nvim',
         config = function()
             require('oil').setup()
         end,
-    }) -- File explorer with editing capabilities
-    use({
+    }, -- File explorer with editing capabilities
+    {
         'nvim-neo-tree/neo-tree.nvim',
+        cmd = 'Neotree',
+        keys = {
+            { '<leader>ft', '<cmd>Neotree toggle<cr>', desc = 'NeoTree ' },
+        },
         branch = 'v3.x',
-        requires = {
+        dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
             'MunifTanjim/nui.nvim',
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
-    })                           -- File tree
-    use('rebelot/heirline.nvim') -- Status, window and tabline
-    use('folke/twilight.nvim')   -- Dim parts of the code you're not working on
-    use({
+    }, -- File tree
+    'rebelot/heirline.nvim', -- Status, window and tabline
+    'folke/twilight.nvim', -- Dim parts of the code you're not working on
+    {
         'folke/zen-mode.nvim',
-        setup = require('zen-mode').setup({
-            window = {
-                backdrop = 1,
-            },
-            plugins = {
-                twilight = {
-                    enabled = true,
-                },
-                tmux = {
-                    enabled = true,
-                },
-                kitty = {
-                    enabled = true,
-                    font = '+10', -- font size increment
-                },
-                gitsigns = {
-                    enabled = true,
-                },
-            },
-        }),
-    })
-    use('bellinitte/uxntal.vim')
-    use('windwp/nvim-ts-autotag')
-    use({
-        'kylechui/nvim-surround',
-        tag = '*', -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require('nvim-surround').setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end,
-    })
-    use('jvgrootveld/telescope-zoxide')
-    use('nanotee/zoxide.vim')
-    use({
+        --init = require('zen-mode').setup({
+        --    window = {
+        --        backdrop = 1,
+        --    },
+        --    plugins = {
+        --        twilight = {
+        --            enabled = true,
+        --        },
+        --        tmux = {
+        --            enabled = true,
+        --        },
+        --        kitty = {
+        --            enabled = true,
+        --            font = '+10', -- font size increment
+        --        },
+        --        gitsigns = {
+        --            enabled = true,
+        --        },
+        --    },
+        --}),
+    },
+    'bellinitte/uxntal.vim',
+    'windwp/nvim-ts-autotag',
+    'jvgrootveld/telescope-zoxide',
+    'nanotee/zoxide.vim',
+    {
         'ckolkey/ts-node-action',
-        requires = { 'nvim-treesitter' },
+        dependencies = { 'nvim-treesitter' },
         config = function()
             require('ts-node-action').setup({})
         end,
-    })
-    use({
-        'tomasky/bookmarks.nvim',
-    })
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if PACKER_BOOTSTRAP then
-        require('packer').update()
-    end
-end)
+    },
+    'tomasky/bookmarks.nvim',
+}
