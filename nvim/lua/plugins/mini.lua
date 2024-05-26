6be55ae08081403fdab173b83fa29441be69597c
zombieman32 <rafaelverde10@gmail.com>
2024-05-25 22:21:29 -0300

    nvim: Added config for mini.map

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -3,6 +3,8 @@ return {
         'echasnovski/mini.nvim',
         verssion = false,
         config = function()
+            local MiniMap = require('mini.map')
+
             require('mini.animate').setup()
             require('mini.cursorword').setup()
             require('mini.surround').setup()
@@ -12,7 +14,19 @@ return {
             require('mini.move').setup()
             require('mini.operators').setup()
             require('mini.trailspace').setup()
-            require('mini.map').setup()
+
+            MiniMap.setup({
+                symbols = {
+                    encode = MiniMap.gen_encode_symbols.shade('2x1'),
+                    scroll_line = ' ',
+                },
+                window = {
+                    winblend = 50,
+                    zindex = 100
+                }
+            })
+            vim.api.nvim_set_keymap('n', '<C-m>', '<cmd>lua require("mini.map").toggle()<cr>', {silent = true, noremap = true})
+
             require('mini.comment').setup()
             require('mini.sessions').setup()
         end,
