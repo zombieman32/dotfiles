72575251883c0fd789dedc97b9cd28d5e7233ddb
zombieman32 <rafaelverde10@gmail.com>
2024-06-26 00:30:36 -0300

    nvim: modicator config fix

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -51,6 +51,7 @@ return {
     -- Change numbercolumn color depending on mode
     {
         'mawkler/modicator.nvim',
+        event = 'ModeChanged',
         dependencies = 'catppuccin', -- Add your colorscheme plugin here
         lazy = true,
         init = function()
@@ -60,7 +61,10 @@ return {
             vim.o.termguicolors = true
         end,
         opts = {
-            show_warnings = false,
+            show_warnings = true,
+            highlights = {
+                defaults = { bold = true },
+            },
         },
     },
 
@@ -147,7 +151,7 @@ return {
                 add_close_pattern = true, -- true, 'last_line' or false
 
                 matchup_patterns = {
-                    { '{',  '}' },
+                    { '{', '}' },
                     { '%(', ')' }, -- % to escape lua pattern char
                     { '%[', ']' }, -- % to escape lua pattern char
                 },
@@ -162,33 +166,32 @@ return {
         'folke/noice.nvim',
         event = 'VeryLazy',
         opts = {
-                presets = {
-                    bottom_search = true,         -- use a classic bottom cmdline for search
-                    command_palette = true,       -- position the cmdline and popupmenu together
-                    long_message_to_split = true, -- long messages will be sent to a split
-                    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = false,       -- add a border to hover docs and signature help
+            presets = {
+                bottom_search = true, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+            },
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                    ['vim.lsp.util.stylize_markdown'] = true,
+                    ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
                 },
-                lsp = {
-                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                    override = {
-                        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                        ['vim.lsp.util.stylize_markdown'] = true,
-                        ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-                    },
-                },
-                -- you can enable a preset for easier configuration
-                popupmenu = {
-                    backend = "cmp",
-                }
+            },
+            -- you can enable a preset for easier configuration
+            popupmenu = {
+                backend = 'cmp',
+            },
         },
         dependencies = {
             'MunifTanjim/nui.nvim',
             'rcarriga/nvim-notify',
         },
         config = function()
-            require('noice').setup({
-            })
+            require('noice').setup({})
         end,
     },
 
