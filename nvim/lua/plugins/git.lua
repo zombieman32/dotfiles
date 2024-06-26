b4c537fc01dee7008140f4f69c1ab824f7e97516
zombieman32 <rafaelverde10@gmail.com>
2024-06-26 00:30:59 -0300

    nvim: Removed deprecated GitSigns configuration

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -6,36 +6,16 @@ return {
 
             gitsigns.setup({
                 signs = {
-                    add = { hl = 'GitSignsAdd', text = '', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-                    change = {
-                        hl = 'GitSignsChange',
-                        text = '',
-                        numhl = 'GitSignsChangeNr',
-                        linehl = 'GitSignsChangeLn',
-                    },
-                    delete = {
-                        hl = 'GitSignsDelete',
-                        text = '',
-                        numhl = 'GitSignsDeleteNr',
-                        linehl = 'GitSignsDeleteLn',
-                    },
-                    topdelete = {
-                        hl = 'GitSignsDelete',
-                        text = '',
-                        numhl = 'GitSignsDeleteNr',
-                        linehl = 'GitSignsDeleteLn',
-                    },
-                    changedelete = {
-                        hl = 'GitSignsChange',
-                        text = '',
-                        numhl = 'GitSignsChangeNr',
-                        linehl = 'GitSignsChangeLn',
-                    },
+                    add = { text = ' ' },
+                    change = { text = ' ' },
+                    delete = { text = ' ' },
+                    topdelete = { text = ' ' },
+                    changedelete = { text = ' ' },
                 },
                 signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-                numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+                numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
                 linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-                word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+                word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
                 watch_gitdir = {
                     interval = 1000,
                     follow_files = true,
@@ -48,9 +28,6 @@ return {
                     delay = 1000,
                     ignore_whitespace = false,
                 },
-                current_line_blame_formatter_opts = {
-                    relative_time = false,
-                },
                 sign_priority = 6,
                 update_debounce = 100,
                 status_formatter = nil, -- Use default
@@ -62,9 +39,6 @@ return {
                     relative = 'cursor',
                     row = 0,
                     col = 1,
-                },
-                yadm = {
-                    enable = false,
                 },
             })
         end,
