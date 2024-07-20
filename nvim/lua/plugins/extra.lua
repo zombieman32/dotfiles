10e5e97438bcda625da903b029ab6fc47eb39dd7
zombieman32 <rafaelverde10@gmail.com>
2024-07-20 10:39:59 -0300

    nvim: Removed and changed deprecated options from which-key plugin

--- nvim/lua/plugins/extra.lua
+++ nvim/lua/plugins/extra.lua
@@ -310,16 +310,14 @@ return {
                     separator = ' ', -- symbol used between a key and it's label
                     group = '󰜌 ', -- symbol prepended to a group
                 },
-                popup_mappings = {
+                keys = {
                     scroll_down = '<c-d>', -- binding to scroll down inside the popup
                     scroll_up = '<c-u>', -- binding to scroll up inside the popup
                 },
-                window = {
+                win = {
                     border = 'rounded', -- none, single, double, shadow
-                    position = 'bottom', -- bottom, top
-                    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
                     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-                    winblend = 30,
+                    title_pos = 'center',
                 },
                 layout = {
                     height = { min = 4, max = 25 }, -- min and max height of the columns
@@ -327,18 +325,9 @@ return {
                     spacing = 3, -- spacing between columns
                     align = 'left', -- align columns left, center or right
                 },
-                ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-                hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
                 show_help = true, -- show help message on the command line when the popup is visible
                 triggers = 'auto', -- automatically setup triggers
                 -- triggers = {"<leader>"} -- or specify a list manually
-                triggers_blacklist = {
-                    -- list of mode / prefixes that should never be hooked by WhichKey
-                    -- this is mostly relevant for key maps that start with a native binding
-                    -- most people should not need to change this
-                    i = { 'j', 'k' },
-                    v = { 'j', 'k' },
-                },
             }
 
             local opts = {
